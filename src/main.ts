44e2791068414b0f30230b8825cd39a58da2a41f
san <>
2023-07-04 14:32:40 +0800

    uview-plus

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { createSSRApp } from 'vue'
-import { createPinia } from 'pinia'
+import uviewPlus from 'uview-plus'
 import App from './App.vue'
 import 'uno.css'
 
 export function createApp() {
   const app = createSSRApp(App)
-  const pinia = createPinia()
-  app.use(pinia)
+  app.use(uviewPlus)
   return {
     app,
   }
